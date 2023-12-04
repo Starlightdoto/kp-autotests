6dffbc2163e1e9ddbb57101760ce69d3c534ab0b
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2023-12-04 23:03:15 +0600

    Added helper variables

--- e2e/first.test.ts
+++ e2e/first.test.ts
@@ -1,14 +1,14 @@
 import {test, expect} from '@playwright/test';
 import { createSBPRequestLink } from './helpers/create_request_link';
 
-test.beforeAll(() => {
-    createSBPRequestLink();
-})
+let baseUrl = '';
+test.beforeAll(async ({page}) => {
+    baseUrl = await createSBPRequestLink({page}, '19', 'send', '500', '643');
 
-
+});
 
 test.describe('SBP page test', async () => {
-    
+
 })
 
 
--- e2e/helpers/create_request_link.ts
+++ e2e/helpers/create_request_link.ts
@@ -1,30 +1,42 @@
 const baseUrl = 'https://api.kiberpay.com/api/test/test';
 
-export const createSBPRequestLink = async ({page}) => {
+const loginLocator = 'body > form > p:nth-child(1) > input[type=text]';
+const passwordLocator = 'body > form > p:nth-child(2) > input[type=password]';
+const login = 'Mylink';
+const password = 'SuperLink';
+const merchantSelectorLocator = 'body > form > p:nth-child(3) > select';
+const versionSelectorLocator = 'body > form > p:nth-child(4) > select';
+const sumLocator = 'body > form > p:nth-child(5) > input[type=text]';
+const currencySelectorLocator = 'body > form > p:nth-child(6) > select';
+const p2pCheckboxLocator = 'id=p2p';
+const sbpCheckboxLocator = 'id=sbp';
+const submitButtonLocator = 'body > form > p:nth-child(43) > input[type=submit]';
+const returnUrl = 'https://google.com';
+
+export const createSBPRequestLink = async ({page}, merchant, version, sum, currency) => {
     await page.goto(baseUrl);
 
+    await page.fill(loginLocator, login);
 
-    await page.fill('body > form > p:nth-child(1) > input[type=text]', 'Mylink');
+    await page.fill(passwordLocator, password);
+
+    await page.selectOption(merchantSelectorLocator, {
+         value: merchant });
     
-    await page.fill('body > form > p:nth-child(2) > input[type=password]', 'SuperLink');
+    await page.selectOption(versionSelectorLocator, {
+         value: version});
+
+    await page.fill(sumLocator, sum);
     
-    await page.selectOption("body > form > p:nth-child(3) > select", {
-         value: "19" });
-    
-        await page.selectOption("body > form > p:nth-child(4) > select", {
-         value: "send"});
-    
-    await page.fill('body > form > p:nth-child(5) > input[type=text]', '1000');
-    
-    await page.selectOption("body > form > p:nth-child(6) > select", {
-             value: "643"});
-             
-    
-    const p2pCheckbox = page.locator ("id=p2p");
-    const sbpCheckbox = page.locator ("id=sbp");
+    await page.selectOption(currencySelectorLocator, {
+             value: currency});
+
+    const sbpCheckbox = page.locator(sbpCheckboxLocator);
+    const p2pCheckbox = page.locator(p2pCheckboxLocator);
     await p2pCheckbox.click();
     await sbpCheckbox.check();
-    await page.click ("body > form > p:nth-child(43) > input[type=submit]");
+    await page.click(submitButtonLocator);
+    return `https://api.kiberpay.com/api/test/inrequest?login=${login}&pas=${password}&id_merch=${merchant}&pspname=${version}&amount=${sum}&currency=${currency}&rate=0&fee=0&amount_edit=true&sbp=true&qr_bank=4&callback_url=&success_url=&fail_url=&return_url=${returnUrl}&merchant_uid=&customer_uid=&customer_acc=&mail=&test=%7B%7D&q=Send`;
 }
 
     
